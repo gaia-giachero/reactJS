b021a2cc7e190cec07546d55afe56d2076535484
gaia-giachero <gaia290106@gmail.com>
2026-09-12 08:21:55 +0000

    feat SearchBar component and focus on input element at the mount of page with useRef

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useRef } from "react"
+
 export default function SearchBar({ searchTerm, setSearchTerm }) {
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
+
     return (
         <div>
             <input
+                ref={inputRef}
                 type="text"
                 placeholder="🔍 Cerca un film"
                 value={searchTerm}
